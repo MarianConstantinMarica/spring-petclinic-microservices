c2b7965e92d0ddccd73126e16cb9ed1fe90ccbab
Marian-Constantin Marica <marian.constantin.marica@gmail.com>
2023-12-20 14:25:13 -0800

    Create spring-petclinic-authorization-server module

---
+++ spring-petclinic-authorization-server/src/test/java/org/springframework/samples/petclinic/springpetclinicauthorizationserver/SpringPetclinicAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.samples.petclinic.springpetclinicauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
